c3de0f6680bfc9ae67681cbf9942023802cc0de1
alvindav <alvind@sfu.ca>
2019-02-09 09:05:32 -0800

    First Commit

---
+++ SpaceInvaders.py
@@ -0,0 +1,15 @@
+#Space Invaders
+import turtle
+import os
+
+
+#Set up the screen
+wn = turtle.Screen()
+wn.bgcolor("black")
+wn.title("Space Invaders")
+
+
+
+
+
+delay = input("Press Enter to Finish")
